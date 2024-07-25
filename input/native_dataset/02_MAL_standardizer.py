1fff81e99180ce6144af6ab6a96049e2ce3a005d
Saverio Mancino <152511017+RavyHollow@users.noreply.github.com>
2024-07-25 15:53:46 +0200

    MAL conversion

---
+++ input/native_dataset/02_MAL_standardizer.py
@@ -0,0 +1,39 @@
+import os
+import geopandas as gpd
+from dotenv import load_dotenv
+
+# Enviroment loading from config.env file -----------------------------------------------------------------------
+
+load_dotenv("../../config.env")
+files_repo = os.getenv("FILES_REPO")
+files_repo_linux = os.getenv("FILES_REPO_LINUX")
+
+# Verify if its there is a Windows G-Drive files repo or a Linux G-Drive files repo
+if os.path.exists(files_repo):
+    root = files_repo
+else:
+    root = files_repo_linux
+
+print(f"Using root= {root}")
+
+# -----------------------------------------------------------------------
+
+# MAL -----------------------------------------------------------------------
+# SHP to CSV
+
+# Leggi il file SHP
+df_orig = gpd.read_file(f"{root}/input/download/2_MAL/Sabah_zenodo/12_MAL.shp")
+
+# Assicurati che il CRS sia EPSG:4326
+df_orig = df_orig.to_crs(epsg=4326)
+
+# Genera il WKT_GEOM per i poligoni
+df_orig['WKT_GEOM'] = df_orig.geometry.apply(lambda geom: geom.wkt)
+# Seleziona tutte le colonne tranne 'geometry'
+columns_to_save = [col for col in df_orig.columns if col != 'geometry']
+
+# Crea un nuovo DataFrame con le colonne selezionate
+df_final = df_orig[columns_to_save]
+
+# Salva il DataFrame come CSV
+df_final.to_csv(f"{root}/input/native_dataset/02_MAL_native.csv", index=False)
