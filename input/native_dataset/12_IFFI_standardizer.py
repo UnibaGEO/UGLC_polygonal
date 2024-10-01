b190afa2ee99d5dca02a128d9059092f57ae3ae4
RavyHollow <mancino.s@hotmail.it>
2024-10-01 17:55:40 +0200

    IFFI standardizer completed

--- input/native_dataset/12_IFFI_standardizer.py
+++ input/native_dataset/12_IFFI_standardizer.py
@@ -1,61 +1,42 @@
 import os
 import geopandas as gpd
 from dotenv import load_dotenv
-import pandas as pd
-from shapely import wkt
 
+# Enviroment loading from config.env file -----------------------------------------------------------------------
 
-# Load the environment variables from config.env file
 load_dotenv("../../config.env")
-root = os.getenv("FILES_REPO")
+files_repo = os.getenv("FILES_REPO")
+files_repo_linux = os.getenv("FILES_REPO_LINUX")
 
-# Lista dei file SHP da unire
-shp_files = [f"{root}/input/download/17_IFFI/frane_piff_abruzzo_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_basilicata_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_bolzano_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_calabria_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_campania_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_emilia-romagna_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_friuli-venezia-giulia_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_lazio_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_liguria_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_lombardia_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_marche_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_molise_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_piemonte_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_puglia_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_sardegna_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_sicilia_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_toscana_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_trento_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_umbria_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_valle-d-aosta_opendata/frane_piff_opendataPoint.shp",
-             f"{root}/input/download/17_IFFI/frane_piff_veneto_opendata/frane_piff_opendataPoint.shp",]
+# Verify if its there is a Windows G-Drive files repo or a Linux G-Drive files repo
+if os.path.exists(files_repo):
+    root = files_repo
+else:
+    root = files_repo_linux
+
+print(f"Using root= {root}")
+
+# -----------------------------------------------------------------------
 
 
-# Leggi il primo file SHP
-merged_data = gpd.read_file(shp_files[0])
+# IFFI -----------------------------------------------------------------------
+# SHP to CSV
 
-# Loop attraverso gli altri file e uniscili al primo
-for shp_file in shp_files[1:]:
-    data_to_merge = gpd.read_file(shp_file)
-    merged_data = gpd.GeoDataFrame(
-        pd.concat([merged_data, data_to_merge], ignore_index=True)
-    )
+# Read the SHP file
+df_orig = gpd.read_file(f"{root}/input/download/12_IFFI/frane_poly_basilicata_opendata/frane_poly_opendataPolygon.shp")
 
-# Salva il file unito
-merged_data.to_file(f"{root}/input/download/17_IFFI/IFFI.shp")
+# Set the CRS as EPSG:4326
+df_orig = df_orig.to_crs(epsg=4326)
 
-# Load the shp
-df_orig = gpd.read_file(f"{root}/input/download/17_IFFI/IFFI.shp")
+# Generate the WKT_GEOM for the polygons
+df_orig['WKT_GEOM'] = df_orig.geometry.apply(lambda geom: geom.wkt)
 
-# Set the CRS to EPSG4326
-crs_shapefile = df_orig.crs
-if crs_shapefile != 'EPSG:4326':
-    df_orig = df_orig.to_crs(epsg=4326)
+# Select all columns except for 'geometry'
+columns_to_save = [col for col in df_orig.columns if col != 'geometry']
 
-# Crete a GeoDataFrame using 'geometry' column for generating the WKT_GEOM
-df_orig['WKT_GEOM'] = df_orig['geometry'].apply(lambda geom: f"POINT ({geom.xy[0][0]} {geom.xy[1][0]})")
+# Create a new DataFrame with the selected columns
+df_final = df_orig[columns_to_save]
 
-# Save the GeoDataFrame as a CSV
-df_orig.to_csv(f"{root}/input/native_datasets/17_IFFI_native.csv", index=False)
+# Save the final DataFrame to a CSV file
+output_path = f"{root}/input/native_dataset/12_IFFI_native.csv"
+df_final.to_csv(output_path, index=False, sep=';', encoding="utf-8")
