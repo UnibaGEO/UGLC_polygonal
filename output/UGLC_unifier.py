8919b5ab5c66301462b1c9955463704f2007a3ca
RavyHollow <mancino.s@hotmail.it>
2024-10-21 14:33:56 +0200

    FEATURE IMPLEMENTATION - added Geopackage format dataset saving option - added splitting into 8x8 global tiles of GeoPackage files - README updated

--- output/UGLC_unifier.py
+++ output/UGLC_unifier.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import geopandas as gpd
 from dotenv import load_dotenv
 
 # Enviroment loading from config.env file -----------------------------------------------------------------------
@@ -42,35 +43,60 @@ df_combined = df_combined[df_combined['TYPE'] != "snow avalanche"]
 df_combined['ID'] = [str(i) for i in range(1, len(df_combined) + 1)]
 
 ## ------------------ DIRECTORY SELECTION ------------------
-## G-Cloud directory
+##           [G-Cloud directory]
 ## (keep this commented when is not used)
-## print(f"> Saving on cloud directory")
+#
+# print(f"> Saving on cloud directory")
 # output_file_root = f"{root}/output"
 
-## Local directory
+##           [Local directory]
 ## (insert your local directory)
 ## (keep this commented when is not used)
+
 print(f"> Saving on local directory")
 output_file_root = f"C:/Users/microzonazione_05/Desktop/UGLC Dataset/UGLC_poly"
+# ------------------------------------
 
 # Verify the csv files presence into the dir
 if csv_files:
     output_file = f"{output_file_root}/UGLC_poly.csv"
     df_combined.to_csv(output_file, index=False, sep='|')
-    print(f"UGLC Dataframe created on '{output_file}' path with '|' as separator.")
+    print(f"UGLC Dataframe created on '{output_file}' path as CSV with '|' as separator.")
 
-# -------TEST SAVING AS .GPKG----------
+# ------------------ TILING AS .GPKG ------------------
 
-    # Convert the dataframe to GeoDataFrame assuming it has geometry in 'WKT_GEOM'
-#    gdf = gpd.GeoDataFrame(df_combined, geometry=gpd.GeoSeries.from_wkt(df_combined['WKT_GEOM']))
-    
-    # Set the CRS to EPSG:4326 (WGS 84), adjust if necessary
-#    gdf.set_crs(epsg=4326, inplace=True)
-    
-    # Save as GeoPackage
-#    output_file_gpkg = f"{output_file_root}/UGLC_poly.gpkg"
-#    gdf.to_file(output_file_gpkg, driver='GPKG')
-#    print(f"UGLC GeoDataFrame created on '{output_file_gpkg}' path as GeoPackage.")
-# -------------------------------------
+    # Convert the dataframe to GeoDataFrame
+    # Define global limits
+    min_lon, max_lon = -180, 180
+    min_lat, max_lat = -90, 90
+
+    # Calculate tile dimension (8x8 grid)
+    lon_step = (max_lon - min_lon) / 8
+    lat_step = (max_lat - min_lat) / 8
+
+    # Convert the dataframe as GeoDataFrame
+    gdf = gpd.GeoDataFrame(df_combined, geometry=gpd.GeoSeries.from_wkt(df_combined['WKT_GEOM']))
+
+    # Set the CRS to EPSG:4326 (WGS 84)
+    gdf.set_crs(epsg=4326, inplace=True)
+
+    # Loop on 8x8 grid's row and columns
+    for i in range(8):
+        for j in range(8):
+            # Tile frame limit
+            tile_min_lon = min_lon + i * lon_step
+            tile_max_lon = tile_min_lon + lon_step
+            tile_min_lat = min_lat + j * lat_step
+            tile_max_lat = tile_min_lat + lat_step
+
+            # Filters polygons that fall within tile boundaries
+            tile_gdf = gdf.cx[tile_min_lon:tile_max_lon, tile_min_lat:tile_max_lat]
+
+            # Save the result as GeoPackage
+            output_file_gpkg = f"{output_file_root}/UGLC_poly_tile_{i}_{j}.gpkg"
+            tile_gdf.to_file(output_file_gpkg, driver='GPKG')
+            print(f"GeoDataFrame Tile ({i}, {j}) saved on '{output_file_gpkg}' as GeoPackage.")
+
+# ------------------------------------
 else:
     print("No CSV file found in to the directory.")
